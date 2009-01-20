ba24d6b90e2db864b0d3eebed30d62e76aacf335
Ricardo SIGNES <rjbs@cpan.org>
2009-01-20 14:19:48 -0500

    for yaml manifests

---
+++ lib/Email/MIME/Kit/ManifestReader/YAML.pm
@@ -0,0 +1,18 @@
+package Email::MIME::Kit::ManifestReader::YAML;
+use Moose;
+
+with 'Email::MIME::Kit::Role::ManifestReader';
+with 'Email::MIME::Kit::Role::ManifestDesugarer';
+
+use YAML::XS ();
+
+sub read_manifest {
+  my ($self) = @_;
+
+  my $yaml_ref = $self->kit->kit_reader->get_kit_entry('manifest.yaml');
+
+  my ($content) = YAML::XS::Load($$yaml_ref);
+}
+
+no Moose;
+1;
