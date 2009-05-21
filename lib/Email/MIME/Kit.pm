f1778574f3a78e2472845de84eb12c3fa6c8d396
Ricardo SIGNES <rjbs@cpan.org>
2009-05-21 08:31:06 -0400

    DESC should be a head1

--- lib/Email/MIME/Kit.pm
+++ lib/Email/MIME/Kit.pm
@@ -31,7 +31,7 @@ before relying on this code in your production environment.
 
   $transport->send($email, { ... });
 
-=head2 DESCRIPTION
+=head1 DESCRIPTION
 
 Email::MIME::Kit is a templating system for email messages.  Instead of trying
 to be yet another templating system for chunks of text, it makes it easy to
