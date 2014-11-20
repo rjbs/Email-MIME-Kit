841a4d757c6be0d2be6f2972d04745fcc6052064
Ricardo Signes <rjbs@cpan.org>
2014-11-19 22:34:00 -0500

    add get_decoded_kit_entry to KitReader for text entities

--- lib/Email/MIME/Kit/Role/KitReader.pm
+++ lib/Email/MIME/Kit/Role/KitReader.pm
@@ -10,12 +10,21 @@ This role also performs L<Email::MIME::Kit::Role::Component>.
 
 Classes implementing this role must provide a C<get_kit_entry> method.  It will
 be called with one parameter, the name of a path to an entry in the kit.  It
-should return a reference to a scalar holding the contents of the named entry.
-If no entry is found, it should raise an exception.
+should return a reference to a scalar holding the contents (as octets) of the
+named entry.  If no entry is found, it should raise an exception.
 
 =cut
 
 requires 'get_kit_entry';
+
+sub get_decoded_kit_entry {
+  my ($self, @rest) = @_;
+  my $content_ref = $self->get_kit_entry(@rest);
+
+  require Encode;
+  my $decoded = Encode::decode('utf-8', $$content_ref);
+  return \$decoded;
+}
 
 no Moose::Role;
 1;
